e427b3b1a6fe63fa12dee9132a1eb35842d29052
hpryshchepa <hpryshchepa@griddynamics.com>
2025-05-30 13:43:46 +0300

    pretify code

--- src/services/http.ts
+++ src/services/http.ts
@@ -14,7 +14,7 @@ export class HTTP {
 
   async get(url: string) {
     const response = await fetch(`${this.apiConfig.path}${url}`);
-  
+
     if (response.ok) {
       const responseData = await response.json();
       this.logger.info(`Status: ${response.status}. Response: ${JSON.stringify(responseData)}`);
